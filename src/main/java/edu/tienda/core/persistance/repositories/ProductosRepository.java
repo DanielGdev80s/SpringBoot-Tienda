88f4fe5ad73c3c2c3f5b542a76a879afe2862c8e
Daniel Gomez <danielgomez1668@hotmail.com>
2024-04-03 11:07:10 -0300

    Agregada consulta nativa para encontrar productos por precio

--- src/main/java/edu/tienda/core/persistance/repositories/ProductosRepository.java
+++ src/main/java/edu/tienda/core/persistance/repositories/ProductosRepository.java
@@ -2,6 +2,7 @@ package edu.tienda.core.persistance.repositories;
 
 import edu.tienda.core.persistance.entities.ProductoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +15,8 @@ public interface ProductosRepository extends JpaRepository<ProductoEntity, Integ
     List<ProductoEntity> findByNombreLike(String nombre);
 
     List<ProductoEntity> findByPrecioGreaterThanAndStockLessThan(Double precio, Integer stock);
+
+    @Query(value = "SELECT * FROM productos WHERE precio < ?1", nativeQuery = true)
+    List<ProductoEntity> findProductosByPrecioLessThan(double precio);
 
 }
